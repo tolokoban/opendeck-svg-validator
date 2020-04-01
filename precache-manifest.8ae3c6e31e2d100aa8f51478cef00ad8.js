d3d90b4f65a8082009b6e522d4c988650aa5193c
Tolokoban <contact@tolokoban.org>
2020-04-01 12:05:39 +0200

    Updates

---
+++ precache-manifest.8ae3c6e31e2d100aa8f51478cef00ad8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b57e9dd24f569cae91bb",
+    "url": "/static/css/main.7737f839.chunk.css"
+  },
+  {
+    "revision": "b57e9dd24f569cae91bb",
+    "url": "/static/js/main.a68b1007.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa450ebcfa8cb8c9dc2f",
+    "url": "/static/css/2.317c46d2.chunk.css"
+  },
+  {
+    "revision": "fa450ebcfa8cb8c9dc2f",
+    "url": "/static/js/2.7fd168be.chunk.js"
+  },
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "/static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "04c7e99b2918bcab6bebc7a5c44459c5",
+    "url": "/static/media/mystery-quest-400.04c7e99b.woff2"
+  },
+  {
+    "revision": "d73f403cb7dedbcc76e42e5d81cb4e9b",
+    "url": "/static/media/wheel.d73f403c.png"
+  },
+  {
+    "revision": "a9f0e8dd8ab358503fea94cd8ff379a0",
+    "url": "/index.html"
+  }
+];
