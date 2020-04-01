03f4c031ea796897af1db79b650a9a553887df99
Tolokoban <contact@tolokoban.org>
2020-04-01 12:21:26 +0200

    Updates

---
+++ precache-manifest.582837823825789a6d8e4c556a29209a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "db54cbb532e4fa01a7d3",
+    "url": "./static/css/main.9228c267.chunk.css"
+  },
+  {
+    "revision": "db54cbb532e4fa01a7d3",
+    "url": "./static/js/main.c48da0ae.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ad3879c559037492249c",
+    "url": "./static/css/2.1b68a02d.chunk.css"
+  },
+  {
+    "revision": "ad3879c559037492249c",
+    "url": "./static/js/2.7fd168be.chunk.js"
+  },
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "./static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "04c7e99b2918bcab6bebc7a5c44459c5",
+    "url": "./static/media/mystery-quest-400.04c7e99b.woff2"
+  },
+  {
+    "revision": "d73f403cb7dedbcc76e42e5d81cb4e9b",
+    "url": "./static/media/wheel.d73f403c.png"
+  },
+  {
+    "revision": "27463916e40c7247b39eb362afb1b7f7",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.897fee3ab49bdb87df3e53cf90a6e750.js"
+  "./precache-manifest.582837823825789a6d8e4c556a29209a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
